2c6ec0fa72ba0476c21eb2a1cada7c6373488abf
Broshen <boshen.cui@gmail.com>
2016-10-08 12:18:37 -0400

    query responds to front end

--- routes/lyrics.js
+++ routes/lyrics.js
@@ -10,15 +10,39 @@ router.get('/search', function(req, res, next) {
 	console.log(typeof(term));
 
 	//search url
-	var url ="https://cse.google.com/cse?cx=partner-pub-1936238606905173%3A1nb2rugz238&cof=FORID%3A10&q=all+boys+and+girls+sing&sa=Search+Lyrics&siteurl=http%3A%2F%2Fsongwordsearch.com%2F&ad=n9&num=10&rurl=http%3A%2F%2Fsongwordsearch.com%2Fsearch.php%3Fcx%3Dpartner-pub-1936238606905173%253A1nb2rugz238%26cof%3DFORID%253A10%26q%3Dall%2Bboys%2Band%2Bgirls%2Bsing%26sa%3DSearch%2BLyrics%26siteurl%3Dhttp%253A%252F%252Fsongwordsearch.com%252F#gsc.tab=0&gsc.q=all%20boys%20and%20girls%20sing&gsc.page=1"
-	
+	var url ="http://api.musixmatch.com/ws/1.1/track.search?apikey=a1583a3ec43dc2a4f4850d111da66cca&q_track="+term+"&q_lyrics="+term;
 
 	request(url, function(error, response, body) {
 		
-		console.log(body);
-		$ = cheerio.load(body);
+		//console.log(body);
+		console.log(typeof(body));
+		var json = JSON.parse(body);
+		console.log(json);
+		
+		   		
+		var resultsarr = json.message.body.track_list;
+		var tracksarr = [];
+		for(var i=0; i<resultsarr.length; i++){
+			console.log(resultsarr[i].track.track_name);
+
+			console.log(resultsarr[i].track.artist_name);
+
+			console.log(resultsarr[i].track.track_spotify_id);
+			console.log(" ");
+
+			tracksarr.push({trackname: resultsarr[i].track.track_name,
+							artistname: resultsarr[i].track.artist_name,
+							spotifyid: resultsarr[i].track.track_spotify_id});
+
+		}
+
+		res.jsonp(tracksarr);
+
+		//res.end();
+		//console.log();
+		// $ = cheerio.load(body);
 	
-		console.log($('.list-search', 'body'));
+		// console.log($('.list-search', 'body'));
 	//  res.send(test);
 	});
  	//res.render('index', { title: 'Express' });
